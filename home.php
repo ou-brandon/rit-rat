4e2f7b019d72176f05075a20d09e90f1a4a06c00
ou-brandon <73049480+ou-brandon@users.noreply.github.com>
2023-10-30 15:14:22 -0400

    Initial boilerplate code

---
+++ home.php
@@ -0,0 +1,67 @@
+<?php
+ require("connect-db-ritrat.php");
+ require("apis.php");
+ ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">  
+  
+  <!-- 2. include meta tag to ensure proper rendering and touch zooming -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- 
+  Bootstrap is designed to be responsive to mobile.
+  Mobile-first styles are part of the core framework.
+   
+  width=device-width sets the width of the page to follow the screen-width
+  initial-scale=1 sets the initial zoom level when the page is first loaded   
+  -->
+  
+  <meta name="author" content="your name">
+  <meta name="description" content="include some description about your page">  
+    
+  <title>Bootstrap example</title>
+  
+  <!-- 3. link bootstrap -->
+  <!-- if you choose to use CDN for CSS bootstrap -->  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  
+  <!-- you may also use W3's formats -->
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  
+  <!-- 
+  Use a link tag to link an external resource.
+  A rel (relationship) specifies relationship between the current document and the linked resource. 
+  -->
+  
+  <!-- If you choose to use a favicon, specify the destination of the resource in href -->
+  <link rel="icon" type="image/png" href="http://www.cs.virginia.edu/~up3f/cs4750/images/db-icon.png" />
+  
+  <!-- if you choose to download bootstrap and host it locally -->
+  <!-- <link rel="stylesheet" href="path-to-your-file/bootstrap.min.css" /> --> 
+  
+  <!-- include your CSS -->
+  <!-- <link rel="stylesheet" href="custom.css" />  -->
+       
+</head>
+
+<body>
+<div class="container">
+  <h1>Rit 🐀 Rat</h1> 
+  
+    <table class="w3-table w3-bordered w3-card-4 center" style="width:90%">
+    <thead>
+    <tr style="background-color:#B0B0B0">
+        <th width="30%">Name        
+        <th width="30%">Major        
+        <th width="30%">Year 
+        <th>&nbsp;</th>
+        <th>&nbsp;</th>
+    </tr>
+    </thead>
+    </table>
+</div>     
+</body>
+</html>
