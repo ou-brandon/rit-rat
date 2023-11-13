be0708b53815e77e84140a1069ba291d118fb29b
ou-brandon <73049480+ou-brandon@users.noreply.github.com>
2023-11-13 16:36:44 -0500

    Front page styling, added navbar

--- home.php
+++ home.php
@@ -79,7 +79,7 @@
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Rit a Rat</a>
+        <a class="nav-link" href="submission.php">Rit a Rat</a>
       </li>
       
     </ul>
