2ee59a93bb6e5fa329bee7f22c4a7a9fa3496beb
ou-brandon <73049480+ou-brandon@users.noreply.github.com>
2023-11-13 21:49:06 -0500

    Added sort by top

--- home.php
+++ home.php
@@ -98,6 +98,17 @@
   </div>
 </nav>
 <div class="container bg-light">
+<div class="btn-group btn-group-toggle" data-toggle="buttons">
+  <label class="btn btn-secondary active">
+    <input type="radio" name="options" id="option1" autocomplete="off" checked> Active
+  </label>
+  <label class="btn btn-secondary">
+    <input type="radio" name="options" id="option2" autocomplete="off"> Radio
+  </label>
+  <label class="btn btn-secondary">
+    <input type="radio" name="options" id="option3" autocomplete="off"> Radio
+  </label>
+</div>
   <?php foreach ($allPostsNew as $post): ?>
     <div class="card my-3" style="width:90%; ">
       <div class="card-body">
